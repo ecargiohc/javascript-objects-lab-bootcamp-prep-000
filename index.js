6d4e32dde7e719053175330d7d2182e017981d97
gc6123@bard.edu <gc6123@bard.edu>
2018-08-24 16:49:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({prop: 1}, {prop2: 2})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  Object [value] = '1, prop2: 2'
+  Object [key] = '1, prop2: 2'
   return Object
 }
