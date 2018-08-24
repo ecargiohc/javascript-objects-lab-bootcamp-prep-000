be933000744c7355d21a351d39ba0a030d81f9b0
gc6123@bard.edu <gc6123@bard.edu>
2018-08-24 16:40:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign ({}, {key: value})
+  return Object.assign({prop: 1}, {prop2: 2})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  Object [key] = 'value'
+  return Object
+}
