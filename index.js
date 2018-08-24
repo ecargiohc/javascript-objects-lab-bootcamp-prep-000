3ae8ad0d181e11433b68c54bce543d5dc85811ea
gc6123@bard.edu <gc6123@bard.edu>
2018-08-24 17:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function deleteFromObjectByKey(object, key) {
   return key 
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
